37f7fe2bbadfccb70369c860bf991593fad988ba
Manuel de Diego <manuelzoco@gmail.com>
2020-03-15 12:20:43 +0100

    fix: Translate module

--- practica-final/libs/region-detail/ui/src/lib/region-detail-ui.module.ts
+++ practica-final/libs/region-detail/ui/src/lib/region-detail-ui.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { RegionDetailComponent } from './components/region-detail/region-detail.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    TranslateModule
   ],
   declarations: [
     RegionDetailComponent,
--- practica-final/libs/region-view/ui/src/lib/region-view-ui.module.ts
+++ practica-final/libs/region-view/ui/src/lib/region-view-ui.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
 
 import { RegionViewComponent } from './components/region-view/region-view.component';
-import { RouterModule } from '@angular/router';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule,
+    TranslateModule
   ],
   declarations: [
     RegionViewComponent,
